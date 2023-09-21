2ba19cede28ecae0e4bee6f914eead2dacc74a85
Pankaj Jaiswal <pankajj112223333@gmail.com>
2023-09-21 13:12:40 +0530

    done with profitable day api

--- trade_journals/urls.py
+++ trade_journals/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
  path('win-percentage/',views.win_percentage),
  path('profitable-pair/',views.profitable_pair),
+ path('profitable-day/',views.profitable_day),
+
 ]
--- trade_journals/views.py
+++ trade_journals/views.py
@@ -36,13 +36,42 @@ def profitable_pair(request):
 
         # Group the trades by currency pair and sum the profits
         profit_by_pair = df.groupby('symbol')['profit_inr'].sum()
-        print(profit_by_pair)
+        
         # Find the currency pair with the highest total profit
         most_profitable_pair = profit_by_pair.idxmax()
         highest_profit = profit_by_pair.max()
 
         return JsonResponse({
             'most_profitable_pair': most_profitable_pair,
+            'highest_profit': highest_profit
+        }, status=status.HTTP_200_OK)
+    except Exception as e:
+        return JsonResponse({'message': 'Error processing the file'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def profitable_day(request):
+    uploaded_file = request.FILES['report']
+
+    try:
+        # Read the CSV file into a pandas DataFrame
+        df = pd.read_csv(uploaded_file)
+
+        # Convert opening_time_utc and profit_inr columns to appropriate data types
+        df['opening_time_utc'] = pd.to_datetime(df['opening_time_utc'])
+        df['profit_inr'] = pd.to_numeric(df['profit_inr'], errors='coerce')
+
+        # Extract the day of the week and map it to the actual day name
+        df['day_of_week'] = df['opening_time_utc'].dt.day_name()
+
+        # Group the trades by day of the week and sum the profits
+        profit_by_day = df.groupby('day_of_week')['profit_inr'].sum()
+        
+        # Find the day of the week with the highest total profit    
+        most_profitable_day = profit_by_day.idxmax()
+        highest_profit = profit_by_day.max()
+
+        return JsonResponse({
+            'most_profitable_day': most_profitable_day,
             'highest_profit': highest_profit
         }, status=status.HTTP_200_OK)
     except Exception as e:
