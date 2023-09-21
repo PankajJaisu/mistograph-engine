4200d42ec2479b44684ec70544399506b53c6b61
Pankaj Jaiswal <pankajj112223333@gmail.com>
2023-09-21 15:40:33 +0530

    fixed issue in wsgi

--- mistograph_core/wsgi.py
+++ mistograph_core/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mistograph_core.settings')
 application = get_wsgi_application()
 app = application #add here
